4c543222403090ebde493ca347ff9a8c8623d61c
Lamhotjm <lamhotjms@gmail.com>
2020-06-18 12:46:12 -0500

    add two sum

---
+++ src/test/java/com/lamhot/app/TwoSumTest.java
@@ -0,0 +1,38 @@
+package com.lamhot.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwoSumTest {
+
+	
+	TwoSum ts;
+	@Before
+	public void setUp() throws Exception {
+		ts = new TwoSum();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		ts = null;
+	}
+	
+	@Test
+	public void testNull() {
+		int [] input =  null;
+		int [] expect = null;
+		Assert.assertArrayEquals(expect,ts.twoSum(input, 28) );
+	}
+
+	@Test
+	public void test() {
+		int [] input =  {15,95,41,11,13,17};
+		int [] expect = {15,13};
+		Assert.assertArrayEquals(expect,ts.twoSum(input, 28) );
+	}
+
+}
