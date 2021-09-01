120f144446d24727b768e464e0c3b38c471652c4
LamhotJM <lamhot.id@gmail.com>
2021-09-01 15:58:33 -0700

    problem 41

---
+++ src/main/java/prep60/binary_search/Q41_35_Search_Insert_position.java
@@ -0,0 +1,69 @@
+package prep60.binary_search;
+
+public class Q41_35_Search_Insert_position {
+	/*
+	 * 35. Search Insert Position
+	 * 
+	 * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+	You must write an algorithm with O(log n) runtime complexity.
+	
+	 
+	
+	Example 1:
+	
+	Input: nums = [1,3,5,6], target = 5
+	Output: 2
+	Example 2:
+	
+	Input: nums = [1,3,5,6], target = 2
+	Output: 1
+	Example 3:
+	
+	Input: nums = [1,3,5,6], target = 7
+	Output: 4
+	Example 4:
+	
+	Input: nums = [1,3,5,6], target = 0
+	Output: 0
+	Example 5:
+	
+	Input: nums = [1], target = 0
+	Output: 0
+	 
+	
+	Constraints:
+	
+	1 <= nums.length <= 104
+	-104 <= nums[i] <= 104
+	nums contains distinct values sorted in ascending order.
+	-104 <= target <= 104
+	 */
+	
+	
+	public int searchInsert(int[] nums, int target) {
+        if(nums.length<=0 || nums==null)
+		{
+			return -1;
+		}
+
+		int low = 0;
+		int high = nums.length - 1;
+		int mid;
+		while (low <= high) {
+
+			mid = low + (high - low) / 2;
+
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] < target) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+
+		return low;
+    }
+
+}
