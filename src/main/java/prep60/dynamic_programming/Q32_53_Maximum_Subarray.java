e85c90b77a2fe30317ee5638a73e65480a57743e
LamhotJM <lamhot.id@gmail.com>
2021-09-01 15:14:56 -0700

    program 33

--- src/main/java/prep60/dynamic_programming/Q32_53_Maximum_Subarray.java
+++ src/main/java/prep60/dynamic_programming/Q32_53_Maximum_Subarray.java
@@ -36,24 +36,24 @@ public class Q32_53_Maximum_Subarray {
 
 	 */
 	
-	 public int maxSubArray(int[] nums) {
-	        
-	        if (nums.length <= 0 || nums == null)
+	public int maxSubArray(int[] nums) {
 
-			{
-				return 0;
-			}
+		if (nums.length <= 0 || nums == null)
 
-			int max_sum = nums[0];
-			int current_max = max_sum;
-			for (int i = 1; i < nums.length; i++) {
-				current_max = Math.max(current_max + nums[i], nums[i]);
-
-				max_sum = Math.max(current_max, max_sum);
-			}
-			return max_sum;
-
+		{
+			return 0;
 		}
+
+		int max_sum = nums[0];
+		int current_max = max_sum;
+		for (int i = 1; i < nums.length; i++) {
+			current_max = Math.max(current_max + nums[i], nums[i]);
+
+			max_sum = Math.max(current_max, max_sum);
+		}
+		return max_sum;
+
+	}
 	        
 
 }
