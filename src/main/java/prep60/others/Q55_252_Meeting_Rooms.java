694da06137b36e756dd6980e9253e19fba2ae07b
LamhotJM <lamhot.id@gmail.com>
2021-09-03 17:40:45 -0700

    problem 56

--- src/main/java/prep60/others/Q55_252_Meeting_Rooms.java
+++ src/main/java/prep60/others/Q55_252_Meeting_Rooms.java
@@ -34,13 +34,18 @@ public class Q55_252_Meeting_Rooms {
 	}
 
 	public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        for (int i = 0; i < intervals.length - 1; i++) {
-            if (intervals[i][1] > intervals[i + 1][0]) {
-                return false;
-            }
-        }
-        return true;
-    }
+		for (int i = 0; i < intervals.length; i++) {
+			for (int j = i + 1; j < intervals.length; j++) {
+				if (overlap(intervals[i], intervals[j])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private boolean overlap(int[] i1, int[] i2) {
+		return (i1[0] >= i2[0] && i1[0] < i2[1]) || (i2[0] >= i1[0] && i2[0] < i1[1]);
+	}
 
 }
