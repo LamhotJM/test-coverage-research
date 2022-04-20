3d9015b598af2a1e89c7d13ec81285522ee0d0f5
lamhotjm <lamhot.id@gmail.com>
2022-04-20 00:13:29 -0700

    solved

--- src/main/java/com/lamhot/app/practice/RemoveDuplicates.java
+++ src/main/java/com/lamhot/app/practice/RemoveDuplicates.java
@@ -2,16 +2,20 @@ package com.lamhot.app.practice;
 
 public class RemoveDuplicates {
 
-	public static void main(String args[]) {
-
-	}
-
 	public int removeDuplicate(int[] nums) {
 		if (nums == null || nums.length <= 0) {
 			return 0;
 		}
 
-		return 1;
+		int index = 1;
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] != nums[i + 1]) {
+				nums[index++] = nums[i + 1];
+			}
+		}
+
+		return index;
 	}
 
 }
