d3c821435616add1f6771ff29243a2f25d55c2ba
lamhotjm <lamhot.id@gmail.com>
2022-05-02 22:51:28 -0700

    smallestStringWithSwaps

--- src/main/java/com/lamhot/app/practice/StringWithSwaps.java
+++ src/main/java/com/lamhot/app/practice/StringWithSwaps.java
@@ -1,6 +1,9 @@
 package com.lamhot.app.practice;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 class StringWithSwaps {
@@ -20,10 +23,44 @@ class StringWithSwaps {
 	}
 
 	public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
-		return null;
+		
+		HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
+        int n = s.length();
 
-		///
-		// bcad
-		// bacd
+        for(int i = 0; i < pairs.size(); i ++){
+            int u = pairs.get(i).get(0);
+            int v = pairs.get(i).get(1);
+            if(!graph.containsKey(u)) graph.put(u, null);
+            if(!graph.containsKey(v)) graph.put(v, null);
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        char[] ans = new char[n];
+        boolean[] visited = new boolean[n];
+        for(int i = 0; i < n; i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            List<Integer> components = new ArrayList<>();
+            List<Character> chars = new ArrayList<>();
+            dfs(graph, s, visited, components, chars, i);
+            Collections.sort(components);
+            Collections.sort(chars);
+            for(int j = 0; j < components.size(); j++){
+                ans[components.get(j)] = chars.get(j);
+            }
+        }
+        return String.valueOf(ans);
 	}
+	
+	private void dfs(HashMap<Integer, ArrayList<Integer>> graph, String s, boolean[] visited , List<Integer> components, List<Character> chars, int curr){
+        visited[curr] = true;
+        components.add(curr);
+        chars.add(s.charAt(curr));
+        if(!graph.containsKey(curr)) return;
+        for(Integer k:  graph.get(curr)){
+            if(visited[k]) continue;
+            dfs(graph, s, visited, components, chars, k);
+        }
+    }
 }
